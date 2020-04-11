eba8f47a2813bcb127596c642f29e95c348518f8
Guo Zhuoning <gzn00417@foxmail.com>
2020-04-11 23:17:43 +0800

    update flight

--- src/planningEntry/FlightSchedule.java
+++ src/planningEntry/FlightSchedule.java
@@ -9,6 +9,7 @@ import timeSlot.*;
  * a flight plan containing information of plane, locations and time slot
  */
 public class FlightSchedule<R> extends CommonPlanningEntry<R> {
+    private static final int ORIGIN = 0, TERMINAL = 1;
 
     public FlightSchedule(Location location, TimeSlot timeSlot) {
         super(location, timeSlot);
@@ -16,4 +17,19 @@ public class FlightSchedule<R> extends CommonPlanningEntry<R> {
         System.out.println("FlightSchedule");
     }
 
+    public String getLocationOrigin() {
+        return super.getLocation().getLocations().get(ORIGIN);
+    }
+
+    public String getLocationTerminal() {
+        return super.getLocation().getLocations().get(TERMINAL);
+    }
+
+    public String getTimeLeaving() {
+        return super.getTimeSlot().getLeaving().get(ORIGIN).toString();
+    }
+
+    public String getTimeArrival() {
+        return super.getTimeSlot().getLeaving().get(TERMINAL).toString();
+    }
 }
