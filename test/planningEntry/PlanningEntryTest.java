506dabadf37912de45fb1fa2617be77440b7bdd5
Guo Zhuoning <gzn00417@foxmail.com>
2020-04-11 23:17:13 +0800

    Update test

--- test/planningEntry/PlanningEntryTest.java
+++ test/planningEntry/PlanningEntryTest.java
@@ -11,8 +11,14 @@ import location.*;
 import timeSlot.*;
 
 public class PlanningEntryTest {
+    /**
+     * testing strategy:
+     * test the planning entry instance:
+     * generate an instance
+     * change from states to states: to ended or cancelled
+     */
     @Test
-    public void testInstance() {
+    public void testFlightScheduleInstance() {
         Plane plane = new Plane("SB250", "A320", 1000, 2.5);
         Location location = new Location(Arrays.asList("Harbin", "Beijing"));
         TimeSlot timeSlot = new TimeSlot(
@@ -20,6 +26,8 @@ public class PlanningEntryTest {
                         LocalDateTime.parse("2020-02-02 12:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))),
                 Arrays.asList(LocalDateTime.parse("2020-01-01 10:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
                         LocalDateTime.parse("2020-02-02 12:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))));
+
+        // run a plan
         PlanningEntry<Plane> planningEntry = PlanningEntry.newPlanningEntry("FlightSchedule", location, timeSlot);
         assertEquals("WAITING", planningEntry.getState().getStrState());
         assertTrue(planningEntry.allocateResource(plane));
@@ -27,5 +35,15 @@ public class PlanningEntryTest {
         assertTrue(planningEntry.start());
         assertEquals("RUNNING", planningEntry.getState().getStrState());
         assertFalse(planningEntry.cancel());
+        assertEquals("RUNNING", planningEntry.getState().getStrState());
+        assertTrue(planningEntry.finish());
+        assertEquals("ENDED", planningEntry.getState().getStrState());
+
+        // cancel a plan
+        PlanningEntry<Plane> planningEntry_ = PlanningEntry.newPlanningEntry("FlightSchedule", location, timeSlot);
+        assertTrue(planningEntry_.allocateResource(plane));
+        assertEquals("ALLOCATED", planningEntry_.getState().getStrState());
+        assertTrue(planningEntry_.cancel());
+        assertEquals("CANCELLED", planningEntry_.getState().getStrState());
     }
 }
