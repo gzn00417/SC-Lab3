030687be9a8a189ce644af68c380046ca73b6052
Guo Zhuoning <gzn00417@foxmail.com>
2020-04-11 23:17:28 +0800

    Update class acti

--- src/planningEntry/ActivityCalendar.java
+++ src/planningEntry/ActivityCalendar.java
@@ -1,10 +1,41 @@
 package planningEntry;
 
+import java.util.Arrays;
+
+import entryState.*;
+import location.*;
+import resource.*;
+import timeSlot.*;
+
 /**
  * 
- * @author guozn
- *
  */
 public class ActivityCalendar<R> extends CommonPlanningEntry<R> {
+    private static final int CONST_INDEX = 0;
 
+    public ActivityCalendar(Location location, TimeSlot timeSlot) {
+        super(location, timeSlot);
+        this.strPlanningEntryType = "ActivityCalendar";
+        System.out.println("ActivityCalendar");
+    }
+
+    public String getStrLocation() {
+        return super.getLocation().getLocations().get(CONST_INDEX);
+    }
+
+    public String getStrBeginningTime() {
+        return super.getTimeSlot().getLeaving().get(CONST_INDEX).toString();
+    }
+
+    public String getStrEndingTime() {
+        return super.getTimeSlot().getArrival().get(CONST_INDEX).toString();
+    }
+
+    public String getOneResource() {
+        return super.getResource().toString();
+    }
+
+    public void setNewLocation(String strNewLocation) {
+        this.location = new Location(Arrays.asList(strNewLocation));
+    }
 }
