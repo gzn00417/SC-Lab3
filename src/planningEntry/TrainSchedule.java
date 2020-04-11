06cb2af6e0a5eebbf2d8ac0e32e0f1b92d3988c0
Guo Zhuoning <gzn00417@foxmail.com>
2020-04-11 23:17:54 +0800

    update train

--- src/planningEntry/TrainSchedule.java
+++ src/planningEntry/TrainSchedule.java
@@ -1,10 +1,31 @@
 package planningEntry;
 
+import entryState.*;
+import location.*;
+import resource.*;
+import timeSlot.*;
+
 /**
- * 
- * @author guozn
- *
+ * a train plan containing several trains
  */
 public class TrainSchedule<R> extends CommonPlanningEntry<R> {
+    public TrainSchedule(Location location, TimeSlot timeSlot) {
+        super(location, timeSlot);
+        this.strPlanningEntryType = "TrainSchedule";
+        System.out.println("TrainSchedule");
+    }
 
+    public String getLocationOfIndex(int index) {
+        return super.getLocation().getLocations().get(index);
+    }
+
+    public String getLeavingTimeOfIndex(int index) {
+        assert (index != super.getTimeSlot().getLeaving().size() - 1);
+        return super.getTimeSlot().getLeaving().get(index).toString();
+    }
+
+    public String getArrivalTimeOfIndex(int index) {
+        assert (index != 0);
+        return super.getTimeSlot().getArrival().get(index).toString();
+    }
 }
