54b3f77c9ca0b50579e44605848442bb2e2b80bb
Guo Zhuoning <gzn00417@foxmail.com>
2020-04-16 11:56:03 +0800

    Board visualization in App

--- src/apps/FlightScheduleApp.java
+++ src/apps/FlightScheduleApp.java
@@ -1,8 +1,8 @@
 package apps;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
 
 import board.*;
 import planningEntry.*;
@@ -10,26 +10,88 @@ import planningEntryCollection.*;
 import resource.*;
 
 public class FlightScheduleApp {
+	private static final int INPUT_ROWS_PER_UNIT = 13;
+	private static final FlightScheduleCollection flightScheduleCollection = new FlightScheduleCollection();
+	private static final FlightBoard board = new FlightBoard();
+
 	public static void main(String[] args) throws Exception {
-		FlightScheduleCollection flightScheduleCollection = new FlightScheduleCollection();
-		BufferedReader bReader = new BufferedReader(
-				new FileReader(new File("data/FlightSchedule/FlightSchedule_1.txt")));
+		readFile("data/FlightSchedule/FlightSchedule_3.txt");
+		//main
+		JFrame mainFrame = new JFrame("Flight Schedule");
+		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		mainFrame.setLayout(new GridLayout(2, 3, 10, 5));
+		mainFrame.setVisible(true);
+		mainFrame.setSize(500, 400);
+		//visualization
+		JButton visualizeButton = new JButton("Visualize");
+		Container visualizePane = mainFrame.getContentPane();
+		visualizePane.add(visualizeButton);
+		visualizeButton.addActionListener((e) -> visualization());
+		//add planning entry
+	}
+
+	public static void readFile(String strFile) throws Exception {
+		BufferedReader bReader = new BufferedReader(new FileReader(new File(strFile)));
 		String line = "";
 		int cntLine = 0;
 		String stringInfo = "";
 		while ((line = bReader.readLine()) != null) {
+			if (line.equals(""))
+				continue;
 			stringInfo = stringInfo.concat(line + "\n");
 			cntLine++;
-			if (cntLine >= 13) {
+			if (cntLine % INPUT_ROWS_PER_UNIT == 0) {
 				FlightSchedule<Resource> flightSchedule = flightScheduleCollection.addPlanningEntry(stringInfo);
 				if (flightSchedule != null)
 					flightScheduleCollection.allocatePlanningEntry(flightSchedule.getPlanningEntryNumber(), stringInfo);
-				cntLine = 0;
 				stringInfo = "";
 			}
 		}
 		bReader.close();
-		FlightBoard board = new FlightBoard();
-		board.printArrivalFlightsOfTime(flightScheduleCollection, "2020-02-01 15:00");
+	}
+
+	public static void visualization() {
+		// frame
+		JFrame visualizeOptionFrame = new JFrame("Visualization");
+		visualizeOptionFrame.setLayout(new GridLayout(3, 1));
+		visualizeOptionFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		visualizeOptionFrame.setVisible(true);
+		visualizeOptionFrame.setSize(500, 200);
+		//current time
+		Container currentTimeContainer = new Container();
+		currentTimeContainer.setLayout(new FlowLayout());
+		currentTimeContainer.add(new JLabel("Current Time (yyyy-MM-dd HH:mm)"));
+		JTextField currentTimeText = new JTextField(16);
+		currentTimeContainer.add(currentTimeText);
+		visualizeOptionFrame.add(currentTimeContainer);
+		//arrival
+		Container visualizeArrivalContainer = new Container();
+		visualizeArrivalContainer.setLayout(new FlowLayout());
+		visualizeArrivalContainer.add(new JLabel("Arrival Airport"));
+		JTextField arrivalAirportText = new JTextField(16);
+		visualizeArrivalContainer.add(arrivalAirportText);
+		JButton arrivalButton = new JButton("Show Arrival Flights");
+		visualizeArrivalContainer.add(arrivalButton);
+		visualizeOptionFrame.add(visualizeArrivalContainer);
+		//leaving
+		Container visualizeLeavingContainer = new Container();
+		visualizeLeavingContainer.setLayout(new FlowLayout());
+		visualizeLeavingContainer.add(new JLabel("Leaving Airport"));
+		JTextField leavingAirportText = new JTextField(16);
+		visualizeLeavingContainer.add(leavingAirportText);
+		JButton leavingButton = new JButton("Show Leaving Flights");
+		visualizeLeavingContainer.add(leavingButton);
+		visualizeOptionFrame.add(visualizeLeavingContainer);
+		//button
+		arrivalButton.addActionListener((e_) -> {
+			String strCurrentTime = currentTimeText.getText();
+			String strAirport = arrivalAirportText.getText();
+			board.visualize(flightScheduleCollection, strCurrentTime, strAirport, FlightBoard.ARRIVAL);
+		});
+		leavingButton.addActionListener((e_) -> {
+			String strCurrentTime = currentTimeText.getText();
+			String strAirport = leavingAirportText.getText();
+			board.visualize(flightScheduleCollection, strCurrentTime, strAirport, FlightBoard.LEAVING);
+		});
 	}
 }
--- src/board/FlightBoard.java
+++ src/board/FlightBoard.java
@@ -3,31 +3,106 @@ package board;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
+import java.util.Vector;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.*;
 
 import planningEntry.*;
 import planningEntryCollection.*;
 import resource.*;
 
-public class FlightBoard {
-    private final static int HOURS_BEFORE_LATER = 1;
+public class FlightBoard extends JFrame {
+    private static final long serialVersionUID = 1L;
+    /**
+     * choose flights within HOURS_RANGE before or later
+     */
+    private static final int HOURS_RANGE = 1;
+    /**
+     * visualization label of arrival
+     */
+    public static final int ARRIVAL = 1;
+    /**
+     * visualization label of leaving
+     */
+    public static final int LEAVING = -1;
 
     public FlightBoard() {
     }
 
-    public void printArrivalFlightsOfTime(FlightScheduleCollection flightScheduleCollection, String strCurrentTime) {
-        System.out.println("              Arriving Flights");
+    /**
+     * show the all arrival flights in current time at the airport("" if client chooses every airport)
+     * @param flightScheduleCollection
+     * @param strCurrentTime
+     * @param strAirportName chosen airport name ("" client it chooses every airport)
+     * @param intFlightType ARRIVAL if visualize the arrival flights, LEAVING if visualize the leaving flights
+     */
+    public void visualize(FlightScheduleCollection flightScheduleCollection, String strCurrentTime,
+            String strAirportName, int intFlightType) {
         Iterator<PlanningEntry<Resource>> iterator = flightScheduleCollection.getAllPlanningEntries().iterator();
+        Vector<Vector<?>> vData = new Vector<>();
+        Vector<String> vName = new Vector<>();
+        String[] columnsNames = new String[] { "Time", "Entry Number", "Origin", "", "Terminal", "State" };
+        for (String name : columnsNames)
+            vName.add(name);
         while (iterator.hasNext()) {
             FlightSchedule<Resource> planningEntry = (FlightSchedule<Resource>) iterator.next();
+            if (!strAirportName.isEmpty()) {
+                if (intFlightType == FlightBoard.ARRIVAL) {
+                    if (!planningEntry.getLocationTerminal().toLowerCase().equals(strAirportName.toLowerCase()))
+                        continue;
+                } else {
+                    if (!planningEntry.getLocationOrigin().toLowerCase().equals(strAirportName.toLowerCase()))
+                        continue;
+                }
+            }
             LocalDateTime currentTime = LocalDateTime.parse(strCurrentTime,
                     DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-            if (planningEntry.getTimeArrival().isBefore(currentTime.plusHours(HOURS_BEFORE_LATER))
-                    && planningEntry.getTimeArrival().isAfter(currentTime.minusHours(HOURS_BEFORE_LATER))) {
-                System.out.printf("%-6s %-6s %-24s %-10s\n", planningEntry.getTimeArrival().toString().substring(11),
-                        planningEntry.getPlanningEntryNumber(),
-                        planningEntry.getLocationOrigin() + "-" + planningEntry.getLocationTerminal(),
-                        planningEntry.getState().getStrState());
+            LocalDateTime scheduleTime = intFlightType == FlightBoard.ARRIVAL ? planningEntry.getTimeArrival()
+                    : planningEntry.getTimeLeaving();
+            if (scheduleTime.isBefore(currentTime.plusHours(HOURS_RANGE))
+                    && scheduleTime.isAfter(currentTime.minusHours(HOURS_RANGE))) {
+                String strScheduleTime = scheduleTime.toString().substring(11);
+                String planningEntryNumber = planningEntry.getPlanningEntryNumber();
+                String locationOrigin = planningEntry.getLocationOrigin();
+                String locationTerminal = planningEntry.getLocationTerminal();
+                String state = planningEntry.getState().getStrState();
+                Vector<String> vRow = new Vector<>();
+                vRow.add(strScheduleTime);
+                vRow.add(planningEntryNumber);
+                vRow.add(locationOrigin);
+                vRow.add("-->");
+                vRow.add(locationTerminal);
+                vRow.add(state);
+                vData.add((Vector<?>) vRow.clone());
             }
         }
+        makeTable(vData, vName);
+    }
+
+    /**
+     * make a table
+     * @param vData
+     * @param vName
+     */
+    private void makeTable(Vector<Vector<?>> vData, Vector<String> vName) {
+        DefaultTableModel dataModel = new DefaultTableModel(vData, vName);
+        JTable jtable = new JTable();
+        jtable.setModel(dataModel);
+        JScrollPane jscrollpane = new JScrollPane(jtable);
+        jtable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        jtable.getColumn("").setPreferredWidth(0);
+        DefaultTableCellRenderer r = new DefaultTableCellRenderer();
+        r.setHorizontalAlignment(JLabel.CENTER);
+        jtable.setDefaultRenderer(Object.class, r);
+        setTitle("Arriving Flights");
+        setBounds(100, 100, 100, 100);
+        setSize(600, 600);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        add(jscrollpane, BorderLayout.CENTER);
     }
 }
