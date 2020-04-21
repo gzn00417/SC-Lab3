842da794e1f58e5f08ee7cb8be3201abbb180340
Guo Zhuoning <gzn00417@foxmail.com>
2020-04-21 20:17:11 +0800

    Update State Enum

--- src/entryState/EntryStateEnum.java
+++ src/entryState/EntryStateEnum.java
@@ -6,16 +6,28 @@ import java.util.Map;
 public enum EntryStateEnum {
     WAITING, ALLOCATED, RUNNING, BLOCKED, ENDED, CANCELLED;
 
-    public static final Map<EntryStateEnum, EntryStateEnum[]> newStateAchievableBlockedAble = new HashMap<>(
-            Map.of(WAITING, new EntryStateEnum[] { ALLOCATED, CANCELLED }, ALLOCATED,
-                    new EntryStateEnum[] { RUNNING, CANCELLED }, RUNNING, new EntryStateEnum[] { BLOCKED, ENDED },
-                    BLOCKED, new EntryStateEnum[] { RUNNING, CANCELLED }, CANCELLED, new EntryStateEnum[] {}, ENDED,
-                    new EntryStateEnum[] {}));
+    public static final Map<EntryStateEnum, EntryStateEnum[]> newStateAchievableBlockedAble = new HashMap<EntryStateEnum, EntryStateEnum[]>() {
+        private static final long serialVersionUID = 1L;
+        {
+            put(WAITING, new EntryStateEnum[] { ALLOCATED, CANCELLED });
+            put(ALLOCATED, new EntryStateEnum[] { RUNNING, CANCELLED });
+            put(RUNNING, new EntryStateEnum[] { BLOCKED, ENDED });
+            put(BLOCKED, new EntryStateEnum[] { RUNNING, CANCELLED });
+            put(CANCELLED, new EntryStateEnum[] {});
+            put(ENDED, new EntryStateEnum[] {});
+        }
+    };
 
-    public static final Map<EntryStateEnum, EntryStateEnum[]> newStateAchievableBlockedDisable = new HashMap<>(
-            Map.of(WAITING, new EntryStateEnum[] { ALLOCATED, CANCELLED }, ALLOCATED,
-                    new EntryStateEnum[] { RUNNING, CANCELLED }, RUNNING, new EntryStateEnum[] { ENDED }, CANCELLED,
-                    new EntryStateEnum[] {}, ENDED, new EntryStateEnum[] {}));
+    public static final Map<EntryStateEnum, EntryStateEnum[]> newStateAchievableBlockedDisable = new HashMap<EntryStateEnum, EntryStateEnum[]>() {
+        private static final long serialVersionUID = 1L;
+        {
+            put(WAITING, new EntryStateEnum[] { ALLOCATED, CANCELLED });
+            put(ALLOCATED, new EntryStateEnum[] { RUNNING, CANCELLED });
+            put(RUNNING, new EntryStateEnum[] { ENDED });
+            put(CANCELLED, new EntryStateEnum[] {});
+            put(ENDED, new EntryStateEnum[] {});
+        }
+    };
 
     public EntryStateEnum[] newStateAchievable(String strPlanningEntryType) {
         if (strPlanningEntryType.contains("Train"))
