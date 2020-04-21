394d30583ead3f70afe3b22d5a74e991d260ea79
Guo Zhuoning <gzn00417@foxmail.com>
2020-04-21 17:38:03 +0800

    Update Activity Calendar Collection

--- src/planningEntry/ActivityCalendar.java
+++ src/planningEntry/ActivityCalendar.java
@@ -3,7 +3,6 @@ package planningEntry;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Comparator;
 import java.util.Objects;
 
 import location.*;
